da638df16cbd5fdd928d426cf07e75510c47bd45
Leco <60701459+L-e-c-o@users.noreply.github.com>
2024-06-22 17:11:35 +0200

    Update mkntsyscall.go

--- mkntsyscall.go
+++ mkntsyscall.go
@@ -603,7 +603,7 @@ func {{.Name}}({{.ParamList}}) {{template "results" .}}{
 
 {{define "funcbody"}}
 func {{.HelperName}}({{.HelperParamList}}) {{template "results" .}}{
-{{template "maybeabsent" .}}	{{template "tmpvars" .}}	{{template "syscall" .}}	return
+{{template "tmpvars" .}}	{{template "syscall" .}}	return
 }
 {{end}}
 
